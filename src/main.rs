905e8411a77331b92dc642de1fd0c8f89c21f79b
lp-peg <tomohiro.katsukura@gmail.com>
2024-10-16 08:56:30 +0900

    Fix: Update launch configuration and main source file
    - Updated `.vscode/launch.json` to improve debugging configuration.
    - Modified `src/main.rs` to fix a bug in the main function.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,51 @@
+use clap::{Parser, Subcommand};
+use std::path::PathBuf;
+
+#[derive(Parser)]
+#[command(version, about, long_about=None)]
+struct Cli {
+    name: Option<String>,
+
+    #[arg(short, long, value_name = "FILE")]
+    config: Option<PathBuf>,
+
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    debug: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Test {
+        #[arg(short, long)]
+        list: bool,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    if let Some(name) = cli.name.as_deref() {
+        println!("Name: {}", name);
+    }
+    if let Some(config_path) = cli.config.as_deref() {
+        println!("Config: {}", config_path.display());
+    }
+    match cli.debug {
+        0 => println!("Debug: off"),
+        1 => println!("Debug: kind of on"),
+        2 => println!("Debug: on"),
+        _ => println!("Don't be crazy"),
+    }
+    match &cli.command {
+        Some(Commands::Test { list }) => {
+            if *list {
+                println!("Test list");
+            } else {
+                println!("Test");
+            }
+        }
+        None => println!("No command"),
+    }
 }
